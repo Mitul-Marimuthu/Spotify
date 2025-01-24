7a962f99bdb6729d7bb68f99bb2617f7415f3d95
Mitul-Marimuthu <mitul6m16@gmail.com>
2025-01-23 18:27:54 -0800

    increased functionality

--- Get_Data.py
+++ Get_Data.py
@@ -15,6 +15,7 @@ REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 
 #global variable to store the authorization code
 auth_code = None
+list = {}
 
 #HTTP server to hadnle the redirect and capture the auth code
 class AuthorizationHandler(BaseHTTPRequestHandler):
@@ -116,110 +117,66 @@ def get_recently_played_tracks(access_token):
 def save_tracks(tracks):
     with open('recently_played_tracks.json', 'w') as json_file:
         json.dump(tracks,json_file, indent=4)
+#reads the current data from the file and loads it into the list dict
+def read_from_file():
+    global list
+    with open('save_data.txt', 'r') as file:
+        line = file.readline()
+        while line:
+            params = line.split('+')
+            list[params[0].strip()] = [params[1].strip(), int(params[2].strip())]
+            line = file.readline()
+    # print('IN LIST: ')
+    # for key in list:
+    #     print(f"{key} + {list[key]}")
 
-def save_in_file(tracks):
-    list = {}
+
+#saves the relevant data into the new file
+def save_new_data(tracks):
+    global list
     for item in tracks['items']:
         track = item['track']
 
         # list[f"{track['name']}, {", ".join(artist['name'] for artist in track['artists'])}"] =  (track['album']['name'], track['album']['images'][0], 0)
         # subset = (track['name'], ", ".join(artist['name'] for artist in track['artists']), track['album']['name'], track['album']['images'][0], 0)
-        if f"{track['name']} - {', '.join(artist['name'] for artist in track['artists'])}" in list:
-            list[f"{track['name']} - {', '.join(artist['name'] for artist in track['artists'])}"][2] += 1
-        else:
-            list[f"{track['name']} - {', '.join(artist['name'] for artist in track['artists'])}"] =  [track['album']['name'], track['album']['images'][0], 1]
 
+        #if the "song - artist" key exists in the lsit, update its listen counter
+        #otherwise add the new song into the list
+        if f"{track['name']} - {', '.join(artist['name'] for artist in track['artists'])}" in list:
+            list[f"{track['name']} - {', '.join(artist['name'] for artist in track['artists'])}"][1] += 1
+        else:
+            list[f"{track['name']} - {', '.join(artist['name'] for artist in track['artists'])}"] =  [f"{track['album']['name']} , {track['album']['images'][0]}", 1]
+
+#sorts the list in descending order by number of times played
+def sort_by_freqeuency():
+    global list
+    list = dict(sorted(list.items(), key=lambda item: item[1][1], reverse=True))
+
+# writes the content of the list into the file in a way that makes it easy to extract
+def write_to_file():
+    global list
     with open('save_data.txt', 'w') as file:
         for item in list:
-            file.write(f"{item}, {', '.join(str(i) for i in list[item])}")
+            #adds a + sign to discern between the three components of the input
+            # 1. "(song name) - (artist)"
+            # 2. "(album name), {image information}"
+            # 3. frequency counter
+            file.write(f"{item} + {list[item][0]} + {list[item][1]}")
             file.write("\n")
 
 if __name__ == "__main__":
+    read_from_file()
     auth_code = get_authorization_code()
     if auth_code:
         access_token = get_access_token(auth_code)
         if access_token:
             tracks = get_recently_played_tracks(access_token)
             if tracks:
-                save_in_file(tracks)
-                print(f"\n\n{len(tracks['items'])}")
-                for item in tracks['items']:
-                    track = item['track']
-                    print(f"{track['name']} by {', '.join(artist['name'] for artist in track['artists'])}")
-
-
-# #clinet_id: 2cf26052dde241b88077957dc40f963b
-# #client secret: 761e102a778348988ecebe405c13a10e
-
-# import requests
-# import json
-# import base64
-# import webbrowser
-# from urllib.parse import urlencode
-
-# #API Credentials
-# CLIENT_ID = '2cf26052dde241b88077957dc40f963b'
-# CLIENT_SECRET = '761e102a778348988ecebe405c13a10e'
-# REDIRECT_URI = 'https://open.spotify.com/'
-
-# #GEt authorization code
-# def get_authorization_code():
-#     auth_url = "https://accounts.spotify.com/authorize"
-#     scopes = "user-read-recently-played"
-#     params = {
-#         'client_id': CLIENT_ID,
-#         'response_type': 'code',
-#         'redirect_uri': REDIRECT_URI,
-#         'scope': scopes,
-#     }
-#     url = f"{auth_url}?{urlencode(params)}"
-#     print(f"Please log in here: {url}")
-#     webbrowser.open(url)
-#     # Manually copy the "code" parameter from the redirected URL and paste it
-#     code = input("Paste the authorization code here: ")
-#     return code
-
-# #get access token
-# def get_access_token(auth_code):
-#     token_url = 'https://accounts.spotify.com/api/token'
-#     #auth_header = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
-
-#     data = {
-#         'grant_type': 'authorization_code',
-#         'code': auth_code,
-#         'redirect_uri': REDIRECT_URI,
-#     }
-#     headers = {
-#         'Authorization': f"Basic {base64.b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode()).decode()}",
-#     }
-
-#     response = requests.post(token_url, data=data, headers=headers)
-#     if response.status_code == 200:
-#         return response.json()['access_token']
-#     else:
-#         print("failed to get access token:", response.json())
-#         return None
-    
-# #get recently played tracks
-# def get_recently_played_tracks(access_token):
-#     url = 'https://api.spotify.com/v1/me/player/recently-played'
-#     headers = {
-#         'Authorization': f'Bearer {access_token}'
-#     }
-#     response = requests.get(url, headers=headers)
-#     if response.status_code == 200:
-#         return response.json()
-#     else:
-#         print("Failed to get recently played tracks:", response.json())
-#         return None
-    
-# if __name__ == "__main__":
-#     auth_code = get_authorization_code()
-#     if auth_code:
-#         access_token = get_access_token(auth_code)
-#         if access_token:
-#             tracks = get_recently_played_tracks(access_token)
-#             if tracks:
-#                 for item in tracks:
-#                     track = item['track']
-#                     print(f"{track['name']} by {', '.join(artist['name'] for artist in track['artists'])}")
+                save_new_data(tracks)
+                sort_by_freqeuency()
+                write_to_file()
+                
+    #             print(f"\n\n{len(tracks['items'])}")
+                # for item in tracks['items']:
+                #     track = item['track']
+                #     print(f"{track['name']} by {', '.join(artist['name'] for artist in track['artists'])}")
